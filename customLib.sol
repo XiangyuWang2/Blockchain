86a5ae888aa3215dee55f1179d9dbcfdfa78f77b
XiangyuWang2 <119439169+XiangyuWang2@users.noreply.github.com>
2022-12-02 14:52:47 +0000

    Update customLib.sol

--- customLib.sol
+++ customLib.sol
@@ -3,7 +3,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 library customLib {
-    address constant owner = 0x8ec42d4D2CbAd10FfD90Ef8033AadFf3d25fbafB;
+    address constant owner = 0x9DA4c8B1918BA29eBA145Ee3616BCDFcFAA2FC51;
 
     function customSend(uint256 value, address receiver) public returns (bool) {
         require(value > 1);
